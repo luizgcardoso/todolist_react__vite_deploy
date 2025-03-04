cb9c76a3ef3b0a8d5149fd48c3734549e607cef4
luizzgcardoso <luizgcardoso21@gmail.com>
2025-03-04 17:07:21 -0300

    reconfiguração de deploy (vite.config.js)

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/todolist_react_vite_deploy",
+  base: "/todolist_react__vite_deploy",
 });
